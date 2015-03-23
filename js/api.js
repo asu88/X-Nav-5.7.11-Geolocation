873f9f6aa47fa965158d287eb40f56d4fc171cce
asu88 <asu.ramon88@gmail.com>
2015-03-23 02:54:09 +0100

    5.9.2

--- js/api.js
+++ js/api.js
@@ -4,3 +4,17 @@ var map = L.map('map').setView([40.2838, -3.8215], 13);
 
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+L.marker([40.2838, -3.8215]).addTo(map)
+    .bindPopup(' Universidad Rey Juan Carlos, Aulario III')
+    .openPopup(); 
+
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup.setLatLng(e.latlng).setContent("Coordenadas " + e.latlng.toString())
+    .openOn(map);
+}
+
+map.on('click', onMapClick);

4c14ae214d82f0f80554307c25d18ae8e841d03f
asu88 <asu.ramon88@gmail.com>
2015-04-11 02:59:30 +0200

    with Modernizr

--- js/api.js
+++ js/api.js
@@ -13,46 +13,35 @@ var map = new ol.Map({
   }); */
 
 
-var map = L.map('map');/*.setView([40.2838, -3.8215], 13); */
+/*.setView([40.2838, -3.8215], 13); */
+
+ if (navigator.geolocation) {
+        var map = L.map('map');
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',      
+             maxZoom: 18
+        }).addTo(map);
 
 
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',      
-     maxZoom: 18
-}).addTo(map);
+        map.locate({setView: true, maxZoom: 16});
 
-/* Para meter el marco 
-L.marker([40.2838, -3.8215]).addTo(map)  
-    .bindPopup(' Universidad Rey Juan Carlos, Aulario III')
-    .openPopup(); */
+        function onLocationFound(e) {
 
-/* Meter un Popup, de tal forma que al clikear sobre el mapa se muestren las coordenadas del sitio 
-var popup = L.popup();
+            var radius = e.accuracy / 2;
 
 
-/* CallBack que se ejecuta al clikear sobre el mapa 
-function onMapClick(e) {
-    popup.setLatLng(e.latlng).setContent("Coordenadas " + e.latlng.toString())
-    .openOn(map);
-}
+            L.marker(e.latlng).addTo(map)
+                .bindPopup("Tu ubicación actual es esta, con una precisión de " + radius + " metros ").openPopup();
 
-map.on('click', onMapClick); */
+            L.circle(e.latlng, radius).addTo(map);
+        }
 
-map.locate({setView: true, maxZoom: 16});
+        map.on('locationfound', onLocationFound); 
 
-function onLocationFound(e) {
-
-    var radius = e.accuracy / 2;
-
-
-    L.marker(e.latlng).addTo(map)
-        .bindPopup("Tu ubicación actual es esta, con una precisión de " + radius + " metros ").openPopup();
-
-    L.circle(e.latlng, radius).addTo(map);
-}
-
-map.on('locationfound', onLocationFound); 
-
-
+ } else {
+           var x = document.getElementById("map");
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    
 
 
